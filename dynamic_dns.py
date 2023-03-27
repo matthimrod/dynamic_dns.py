9787116e050cad29b8a4776253ecced48ddaba97
Matt Himrod <matt@matthimrod.com>
2023-03-27 17:50:47 -0400

    Style changes

--- dynamic_dns.py
+++ dynamic_dns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 import logging
-import os
 import re
 import requests
 import smtplib
@@ -12,35 +11,42 @@ import yaml
 CONFIG_FILENAME = "dynamic_dns.yml"
 
 config = []
-with open(CONFIG_FILENAME, 'r') as stream:
+with open(CONFIG_FILENAME, 'r') as file:
     try:
-        config = yaml.safe_load(stream)
+        config = yaml.safe_load(file)
     except yaml.YAMLError as exc:
         print(exc)
 
-logging.basicConfig(filename=config['config']['log_filename'], encoding='utf-8', level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
+logger = logging.getLogger()
+handler = logging.StreamHandler()
+formatter = logging.Formatter(
+        '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.DEBUG)
+
 
 def email(message):
-    email_text = f"""\
-From: {config['config']['email_from']}
-To: {config['config']['email_to']}
-Subject: Dynamic DNS error.
-
-{message}
-"""
+    email_text = f"From: {config['config']['email_from']}\n" + \
+                 f"To: {config['config']['email_to']}\n" + \
+                 "Subject: Dynamic DNS error.\n\n" + \
+                 message + "\n"
 
     try:
-        # Create a secure SSL context
         context = ssl.create_default_context()
-
-        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as server:
-            server.login(config['config']['username'], config['config']['password'])
-            server.sendmail(config['config']['email_from'], config['config']['email_to'], email_text)
-            server.close()
-
-        logging.warn('Email sent to %s.', config['config']['email_to'])
+        with smtplib.SMTP_SSL('smtp.gmail.com', 
+                              465, 
+                              context=context) as smtp_server:
+            smtp_server.login(config['config']['username'], 
+                              config['config']['password'])
+            smtp_server.sendmail(config['config']['email_from'], 
+                                 config['config']['email_to'], 
+                                 email_text)
+            smtp_server.close()
+        logger.warn('Email sent to %s.', config['config']['email_to'])
+        
     except:
-        logging.error('Something went wrong while sending the failure email.')
+        logger.error('Something went wrong while sending the failure email.')
 
 def get_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -67,7 +73,8 @@ def get_public_ip(api_url):
 
 
 for site in config['sites']:
-    if not (('last_result' in config['sites'][site]) and (config['sites'][site]['last_result'] != 'error')):
+    if not (('last_result' in config['sites'][site]) and \
+            (config['sites'][site]['last_result'] != 'error')):
         payload = {}
         payload['hostname'] = site
         if 'use_local_ip' in config['sites'][site]:
@@ -75,13 +82,17 @@ for site in config['sites']:
         if 'ip_api_url' in config['sites'][site]:
             payload['myip'] = get_public_ip(config['sites'][site]['ip_api_url'])
 
-        result = requests.get(config['config']['api_url'], auth=(config['sites'][site]['username'], config['sites'][site]['password']), params=payload)
-        logging.info('%s: %s', site, result.text)
+        result = requests.get(config['config']['api_url'], 
+                              auth=(config['sites'][site]['username'], 
+                                    config['sites'][site]['password']), 
+                              params=payload)
+        logger.info('%s: %s', site, result.text)
 
-        if (result.status_code != requests.codes.ok) or not (re.match('(good|nochg)', result.text)):
-            logging.error(f'DNS API call failed for {site}. {result.text}')
+        if (result.status_code != requests.codes.ok) or \
+            not (re.match('(good|nochg)', result.text)):
+            logger.error(f'DNS API call failed for {site}. {result.text}')
             email(f'DNS API call failed for {site}. {result.text}')
             config['sites'][site]['last_result'] = 'error'
 
-with open(CONFIG_FILENAME, 'w') as stream:
-    yaml.dump(config, stream, default_flow_style=False)
+with open(CONFIG_FILENAME, 'w') as file:
+    yaml.dump(config, file, default_flow_style=False)
